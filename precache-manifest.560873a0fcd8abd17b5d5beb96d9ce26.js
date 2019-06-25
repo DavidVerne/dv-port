0ef62d32d98d327d51caf11a460f28cbb2562f23
David Verne <davidverne@davids-air.home>
2019-06-25 17:02:39 +0100

    Updates

---
+++ precache-manifest.560873a0fcd8abd17b5d5beb96d9ce26.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c17f54eb994cac0fa57aca9262a690a",
+    "url": "/dv-port/index.html"
+  },
+  {
+    "revision": "1b648e4021b754dc07b0",
+    "url": "/dv-port/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "992a57d91bcc5d4a65fa",
+    "url": "/dv-port/static/css/main.8d3be482.chunk.css"
+  },
+  {
+    "revision": "1b648e4021b754dc07b0",
+    "url": "/dv-port/static/js/2.2e6f954d.chunk.js"
+  },
+  {
+    "revision": "992a57d91bcc5d4a65fa",
+    "url": "/dv-port/static/js/main.a92de72d.chunk.js"
+  },
+  {
+    "revision": "9d6f2f968855d99456f2",
+    "url": "/dv-port/static/js/runtime~main.d7bfdf03.js"
+  },
+  {
+    "revision": "ab3d417e3b2001955cdab382add6cd2b",
+    "url": "/dv-port/static/media/battleship.ab3d417e.png"
+  },
+  {
+    "revision": "909d100c96b5bcc051eb58380445dc3e",
+    "url": "/dv-port/static/media/coffey.909d100c.png"
+  },
+  {
+    "revision": "75b55d320759d5d709dc8056229a6c3a",
+    "url": "/dv-port/static/media/cordapp.75b55d32.png"
+  },
+  {
+    "revision": "d3bcd4093f52ce1748d25bdda122095d",
+    "url": "/dv-port/static/media/crypto.d3bcd409.png"
+  },
+  {
+    "revision": "eb47bd03ebf3f38eb6dc72448a63d5ee",
+    "url": "/dv-port/static/media/cyberpunk.eb47bd03.png"
+  },
+  {
+    "revision": "20ae9f68bf055e20214c63c03937ca44",
+    "url": "/dv-port/static/media/david.20ae9f68.jpg"
+  },
+  {
+    "revision": "b48e3a2c8c9ff84dcae9718b08c064cd",
+    "url": "/dv-port/static/media/david.b48e3a2c.png"
+  },
+  {
+    "revision": "336ff268a19c0302b87abbec59c6d591",
+    "url": "/dv-port/static/media/freeukgen.336ff268.png"
+  },
+  {
+    "revision": "36e782c8961e36943655eb67cb5f4131",
+    "url": "/dv-port/static/media/github.36e782c8.svg"
+  },
+  {
+    "revision": "0183c03166157057055a8db6694e880b",
+    "url": "/dv-port/static/media/linkedin.0183c031.svg"
+  },
+  {
+    "revision": "b8996c77096e007281814d4fb135fb05",
+    "url": "/dv-port/static/media/rollerball.b8996c77.png"
+  },
+  {
+    "revision": "75ea1d275c48d75efa32782bf9113461",
+    "url": "/dv-port/static/media/triton.75ea1d27.png"
+  },
+  {
+    "revision": "3251f43857ce0252301029990e83b131",
+    "url": "/dv-port/static/media/twitter.3251f438.svg"
+  }
+]);
